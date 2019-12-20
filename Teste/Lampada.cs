1eb75fb4f48db24c9b8ae77223ac6a389822734f
Silas Salema <silazdasilva20@gmail.com>
2019-12-20 10:51:17 -0300

    Interruptores para acender lampadas

---
+++ Teste/Lampada.cs
@@ -0,0 +1,74 @@
+﻿namespace Teste
+{
+    class Lampada
+    {
+        public bool L1 { get; set; }
+        public bool L2 { get; set; }
+
+        public void AcionarInteruptor1()
+        {
+            if (L1 == true)
+            {
+                L1 = false;
+            }
+            else
+            {
+                L1 = true;
+            }
+        }
+
+        public void AcionarInteruptor2()
+        {
+            if (L1 == true)
+            {
+                L1 = false;
+            }
+            else
+            {
+                L1 = true;
+            }
+            if (L2 == true)
+            {
+                L2 = false;
+            }
+            else
+            {
+                L2 = true;
+            }
+        }
+        public void VerificarInteruptor(int i)
+        {
+            if (i == 1)
+            {
+                AcionarInteruptor1();
+            }
+            else
+            {
+                AcionarInteruptor2();
+            }
+        }
+        public int ValorFinalL1()
+        {
+            if(L1 == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public int ValorFinalL2()
+        {
+            if (L2 == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe quantas vezes os interuptores serão usados:");
+            int quantidade = int.Parse(Console.ReadLine());
+            Lampada h = new Lampada();
+
+            for(int i = 0; i <quantidade; i++)
+            {
+                Console.Write("Informe o interruptor: ");
+                h.VerificarInteruptor(int.Parse(Console.ReadLine()));
+            }
+
+            int l1 =  h.ValorFinalL1();
+            int l2 = h.ValorFinalL2();
+
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+
+        }
+    }
+}
